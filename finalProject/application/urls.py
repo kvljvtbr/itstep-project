1a5af102eadf6f91e86461a812332b724d5e39ee
tiborkovaljov <tiborkovaljov@gmail.com>
2024-04-22 17:31:37 +0200

    progress

--- finalProject/application/urls.py
+++ finalProject/application/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.loginpage, name='login'),
     path('delete/<str:task_name>/', views.delete_task, name='delete'),
-    path('update/<str:task_name>/', views.update_task, name='update')
+    path('update/<str:task_name>/', views.update_task, name='update'),
+    path('print-users-todos/', views.print_users_tasks, name='print_users_todos'),
+    path('deleted-tasks/', views.deleted_tasks, name='deleted_tasks'),
+    path('in-progress-tasks/', views.in_progress_tasks, name='in_progress_tasks'),
+    path('completed-tasks/', views.completed_tasks, name='completed_tasks'),
 ]
--- finalProject/application/views.py
+++ finalProject/application/views.py
@@ -32,7 +32,7 @@ def register(request):
         password = request.POST.get('password')
 
         if len(password) < 5:
-            messages.error(request, 'Password is too short. It must be at least 5 characters long.')
+            messages.error(request, 'Password is too short. The password must be at least 5 characters long.')
             return redirect('register')
 
         users_by_username = User.objects.filter(username=username)
@@ -80,3 +80,33 @@ def update_task(request, task_name):
     todo.status = True
     todo.save()
     return redirect('home')
+
+
+def print_users_tasks(request):
+    if request.method == 'POST':
+        users_with_todos = []
+        users = User.objects.prefetch_related('todo_set').all()
+
+        for user in users:
+            todos = user.todo_set.all()
+            users_with_todos.append({'user': user, 'todos': todos})
+
+        context = {
+            'users_with_todos': users_with_todos
+        }
+
+        return render(request, 'users_todos.html', context)
+
+    return render(request, 'users_todos.html')
+
+
+def deleted_tasks(request):
+    pass
+
+
+def in_progress_tasks(request):
+    pass
+
+
+def completed_tasks(request):
+    pass
